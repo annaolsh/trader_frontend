2250d5d6b9f667133735eb44feedc80c2dc6c2f2
github username <github email address>
2017-06-08 19:35:54 -0400

    added Shares

--- src/components/TradeGame.js
+++ src/components/TradeGame.js
@@ -5,7 +5,8 @@ export default (props) => {
   return(
     <div>
       <button onClick={props.generator}>Play!</button>
-      <Chart data={props.data}/>
+      <Chart data={props.state.data}/>
+      <h2>{props.state.user.shares} shares</h2>
       <button onClick={props.buy}>Buy!</button>
       <button onClick={props.sell}>Sell!</button>
     </div>
